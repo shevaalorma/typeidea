ed3fa88268904f90369e2cd88ad9e5b781435be8
shevaalorma <281366910@qq.com>
2020-04-18 15:52:37 +0800

    04.18

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -56,10 +56,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'typeidea.urls'
 
+THEME = 'default'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'themes','THEME','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,6 +75,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'typeidea.wsgi.application'
+
 
 
 # Database
